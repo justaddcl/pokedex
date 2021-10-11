93ad2242af783844fce15ff56538a5c675e886b5
Yuji Nelson <k.yuji.nelson@gmail.com>
2021-10-11 23:05:30 +0100

    updated App to show basic UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import SearchForm from './components/SearchForm';
+import usePokemonData from './hooks/usePokemonData';
+import Pokemon from './components/Pokemon';
 
 const App = () => {
-  return <h1>Hello</h1>;
+  const { pokemonData, getPokemonData, loading, error } = usePokemonData();
+  return (
+    <main>
+      <SearchForm handleSubmit={getPokemonData} />
+      {loading && <p>Loading...</p>}
+      {error && <p>Uh oh... {error}</p>}
+      <article>{pokemonData && <Pokemon pokemon={pokemonData} />}</article>
+    </main>
+  );
 };
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
+import SearchForm from './components/SearchForm';
 
-test('renders greeting', () => {
+test('renders search results', () => {
+  const searchTerm = 'pikachu';
   render(<App />);
-  const linkElement = screen.getByText(/hello/i);
-  expect(linkElement).toBeInTheDocument();
+
+  userEvent.type(screen.getByRole('textbox'), searchTerm);
+  userEvent.click(screen.getByRole('button', { name: /search/i }));
+
+  setTimeout(() => {
+    expect(screen.getByRole('heading').textContent).toBe(searchTerm);
+    expect(screen.getByText(/types/i)).toBeVisible();
+    expect(screen.getByText(/abilities/i)).toBeVisible();
+    expect(screen.getByText(/appears in/i)).toBeVisible();
+  }, 500);
 });
